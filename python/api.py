0ff414768c73ba6a183354e56f573256961d356f
mdatsev <martin.datsev.p@gmail.com>
2023-04-02 00:18:42 +0300

    feat: add tiling

--- python/api.py
+++ python/api.py
@@ -15,6 +15,24 @@ pipe = StableDiffusionPipeline.from_pretrained(
 )
 pipe = pipe.to("cuda")
 
+
+original_conv2d_init = None
+
+def patch_conv(cls, use_circular_padding):
+    global original_conv2d_init
+
+    if original_conv2d_init is None:
+        original_conv2d_init = cls.__init__
+
+    def new_init(self, *args, **kwargs):
+        return original_conv2d_init(self, *args, **kwargs, padding_mode='circular')
+
+    if use_circular_padding:
+        cls.__init__ = new_init
+    else:
+        cls.__init__ = original_conv2d_init
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -22,6 +40,11 @@ app = FastAPI()
 @app.post("/")
 async def process_data(input_data: dict):
     print("model request:", input_data)
+
+    if input_data.get("tiling", False):
+        patch_conv(torch.nn.Conv2d, True)
+    else:
+        patch_conv(torch.nn.Conv2d, False)
 
     generated = pipe(
       prompt=input_data.get("prompt", "Astronaut riding a horse"),
@@ -46,5 +69,3 @@ async def process_data(input_data: dict):
         names.append(name)
 
     return { "filenames": names }
-
-
